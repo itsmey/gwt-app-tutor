fa7b38f4776e1ba8551962ec9b895d2eaa143ba8
Ivan Mic <ivan.micr@gmail.com>
2016-09-01 14:39:54 +0500

    add template generated from maven archetype

---
+++ src/main/java/com/ivan/client/gin/ClientModule.java
@@ -0,0 +1,25 @@
+package com.ivan.client.gin;
+
+import com.ivan.client.application.ApplicationModule;
+import com.ivan.client.place.NameTokens;
+import com.ivan.client.resources.ResourceLoader;
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
+
+public class ClientModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        install(new DefaultModule
+                .Builder()
+                .defaultPlace(NameTokens.HOME)
+                .errorPlace(NameTokens.HOME)
+                .unauthorizedPlace(NameTokens.HOME)
+                .build());
+        install(new ApplicationModule());
+
+        bind(ResourceLoader.class).asEagerSingleton();
+    }
+}
---
+++ src/main/java/com/ivan/client/place/NameTokens.java
@@ -0,0 +1,5 @@
+package com.ivan.client.place;
+
+public class NameTokens {
+    public static final String HOME = "/";
+}
