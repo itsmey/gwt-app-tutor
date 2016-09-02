e0d16f66ba9c57546b1f202c1f08fa8d2b651395
Ivan Mic <ivan.micr@gmail.com>
2016-09-02 16:29:04 +0500

    home: add uihandler interface, modify view

---
+++ src/main/java/com/ivan/client/application/home/HomeUiHandlers.java
@@ -0,0 +1,7 @@
+package com.ivan.client.application.home;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+
+public interface HomeUiHandlers extends UiHandlers {
+    void myOnClick(String name);
+}
--- src/main/java/com/ivan/client/application/home/HomeView.java
+++ src/main/java/com/ivan/client/application/home/HomeView.java
@@ -2,16 +2,33 @@ package com.ivan.client.application.home;
 
 import javax.inject.Inject;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.TextBox;
 import com.gwtplatform.mvp.client.ViewImpl;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 
-public class HomeView extends ViewImpl implements HomePresenter.MyView {
+public class HomeView extends ViewWithUiHandlers<HomeUiHandlers> implements HomePresenter.MyView {
     interface Binder extends UiBinder<Widget, HomeView> {
     }
+
+    @UiField
+    TextBox myTextBox;
+
+    @UiField
+    Button myButton;
 
     @Inject
     HomeView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    @UiHandler("myButton")
+    public void myOnClick(ClickEvent event) {
+        getUiHandlers().myOnClick(myTextBox.getText());
     }
 }
