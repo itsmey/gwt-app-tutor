640c519be7e2412db19d467ed82295720e76423c
Ivan Mic <ivan.micr@gmail.com>
2016-09-07 16:50:04 +0500

    add popup info widget

--- src/main/java/com/ivan/client/application/login/LoginPresenter.java
+++ src/main/java/com/ivan/client/application/login/LoginPresenter.java
@@ -3,6 +3,10 @@ package com.ivan.client.application.login;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.ivan.client.application.ApplicationPresenter;
 import com.ivan.client.place.NameTokens;
@@ -14,9 +18,20 @@ import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 
+
 public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresenter.MyProxy>
         implements LoginUiHandlers {
     interface MyView extends View, HasUiHandlers<LoginUiHandlers> {
+    }
+
+    private static class InfoPopup extends PopupPanel {
+        public InfoPopup() {
+            super(true);
+            VerticalPanel panel = new VerticalPanel();
+            panel.add(new Label("This screen is intended to contain some useful information."));
+            panel.add(new Label("You may click somewhere outside to close it."));
+            setWidget(panel);
+        }
     }
 
     @ProxyStandard
@@ -40,6 +55,15 @@ public class LoginPresenter extends Presenter<LoginPresenter.MyView, LoginPresen
 
     @Override
     public void showInfoButtonClick() {
+        final InfoPopup popup = new InfoPopup();;
+
+        popup.setPopupPositionAndShow(new PopupPanel.PositionCallback() {
+            public void setPosition(int offsetWidth, int offsetHeight) {
+                int left = (Window.getClientWidth() - offsetWidth) / 2;
+                int top = (Window.getClientHeight() - offsetHeight) / 2;
+                popup.setPopupPosition(left, top);
+            }
+        });
     }
 
     @Override
