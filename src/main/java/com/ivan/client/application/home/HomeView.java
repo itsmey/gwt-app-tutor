685b2e4382866860fa310749702cfa00b99fed6c
Ivan Mic <ivan.micr@gmail.com>
2016-09-08 16:02:53 +0500

    add widgets to home view

--- src/main/java/com/ivan/client/application/home/HomeView.java
+++ src/main/java/com/ivan/client/application/home/HomeView.java
@@ -6,10 +6,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.*;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 
@@ -17,9 +14,43 @@ public class HomeView extends ViewWithUiHandlers<HomeUiHandlers> implements Home
     interface Binder extends UiBinder<Widget, HomeView> {
     }
 
+    @UiField
+    TextBox nameTextBox;
+
+    @UiField
+    Button doButton;
+
+    @UiField
+    RadioButton crfRadio;
+
+    @UiField
+    RadioButton crdRadio;
+
+    @UiField
+    RadioButton dlfRadio;
+
+    @UiField
+    RadioButton dldRadio;
+
+    @UiField
+    RadioButton vwfRadio;
+
     @Inject
     HomeView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    @UiHandler("doButton")
+    public void doButtonClick(ClickEvent event) {
+        int action_code = 0;
+
+        if (crfRadio.getValue()) action_code = 1;
+        if (crdRadio.getValue()) action_code = 2;
+        if (dlfRadio.getValue()) action_code = 3;
+        if (dldRadio.getValue()) action_code = 4;
+        if (vwfRadio.getValue()) action_code = 5;
+
+        getUiHandlers().doButtonClick(action_code, nameTextBox.getText());
     }
 
 }
